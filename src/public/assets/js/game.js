52c9dea9641efb9ed942bd889a0c3f45490d383a
ergoodwill <35115515+ergoodwill@users.noreply.github.com>
2020-04-23 01:29:28 -0600

    Heroku deployed version

---
+++ src/public/assets/js/game.js
@@ -0,0 +1,380 @@
+/* Adapted from https://phasertutorials.com/how-to-create-a-phaser-3-mmorpg-part-1/ */
+class BootScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'BootScene',
+      active: true
+    });
+  }
+
+  preload() {
+    this.load.image('ball', 'assets/images/ball.png');
+    this.load.image('player', 'assets/images/paddle.png');
+  }
+
+  create() {
+    this.scene.start('WorldScene');
+  }
+}
+
+var cursors;
+var ball;
+var ballOldPosition;
+var bottomScore;
+var topScore;
+var bottomScoreText;
+var topScoreText;
+var bottom;
+var gameWon;
+var score;
+var timer;
+var clickButton;
+var firstID;
+var secondID;
+var offset;
+var direction;
+var ready;
+var ballX;
+var ballY;
+var updateX;
+var updateY;
+var ballFirstHit;
+var notified;
+var caught;
+var pointWon;
+
+class WorldScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'WorldScene'
+    });
+  }
+
+  create() {
+    pointWon = false;
+
+    notified = false;
+    //ballFirstHit = false;
+    ready = false;
+    this.socket = io();
+    caught = false;
+
+    this.otherPlayers = this.physics.add.group();
+    
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    gameWon = false;
+    
+    topScore =0;
+	  topScoreText = this.add.text(16,13,'Score: 0', {fontSize: '32px',fill:'#FFF'});
+	  bottomScore =0;
+    bottomScoreText = this.add.text(16,559, 'Score: 0', {fontSize: '32px',fill:'#FFF'});
+
+    // listen for web socket events
+    this.socket.on('updatedBall', function(ballInfo) {
+      updateX = ballInfo.x;
+      updateY = ballInfo.y;
+      //console.log('ball was updated:', updateX, updateY);
+    }.bind(this));
+
+    this.socket.on('currentPlayers', function (players) {
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === this.socket.id) {
+          bottom = players[id].bottom;
+          this.createPlayer(players[id]);
+        } else {
+          this.addOtherPlayers(players[id]);
+        }
+      }.bind(this));
+    }.bind(this));
+  
+    this.socket.on('newPlayer', function (playerInfo) {
+      this.addOtherPlayers(playerInfo);
+    }.bind(this));
+
+    this.socket.on('disconnect', function (playerId) {
+      this.otherPlayers.getChildren().forEach(function (player) {
+        if (playerId === player.playerId) {
+          player.destroy();
+        }
+      }.bind(this));
+    }.bind(this));
+
+    this.socket.on('playerMoved', function (playerInfo) {
+      this.otherPlayers.getChildren().forEach(function (player) {
+        if (playerInfo.playerId === player.playerId) {
+          player.setPosition(playerInfo.x, playerInfo.y);
+        }
+      }.bind(this));
+    }.bind(this));
+
+    this.socket.on('newBall', function(ballInfo) {
+      this.createBall(ballInfo);
+    }.bind(this));
+
+    this.socket.on('ballMoved', function (movementData) {
+      if(ball) {
+        ball.setPosition(movementData.x, movementData.y);
+      }
+    }.bind(this));
+
+    this.socket.on('firstPlayer', function(id) {
+      firstID = id;
+      console.log('first: ',id);
+    }.bind(this));
+
+    this.socket.on('secondPlayer', function(id) {
+      secondID = id;
+      console.log('second: ',id);
+    }.bind(this));
+
+    this.socket.on('highScore', function(highScore) {
+      if(score > highScore.highScore) {
+        console.log('higher');
+      } else {
+        console.log('lower');
+      }
+    }.bind(this));
+
+    this.socket.on('buttonGone', function () {
+      clickButton.destroy();
+    }.bind(this));
+
+    this.socket.on('autoRestart', function(velocity) {
+      if (ball) {
+        ballFirstHit = false;
+        caught = false;
+        console.log("x,y velocity",velocity.x,velocity.y);
+        ball.setVelocity(velocity.x,velocity.y);
+        if ( velocity.bottom ) {
+          if(!pointWon) {
+            bottomScore+= 0.25;
+            bottomScoreText.setText('Score: ' + Math.floor(bottomScore));
+          }
+        } else {
+          if (!pointWon) {
+            topScore+= 0.25;
+            topScoreText.setText('Score: ' + Math.floor(topScore));
+          }
+        }
+      }
+    }.bind(this));
+
+    this.socket.on('ballHit', function(velocity) {
+      offset=velocity.offsetProduced;
+      direction=velocity.directionProduced;
+      ready = true;
+    }.bind(this));
+
+    this.socket.on('gameEnded', function() {
+      this.endGame();
+    }.bind(this)); 
+
+    this.socket.on('switch', function() {
+      notified = false;
+    }.bind(this));
+
+    this.socket.on('pointAwarded', function() {
+      pointWon = true;
+    }.bind(this));
+    cursors = this.input.keyboard.createCursorKeys();
+    clickButton = this.add.text(300, 300, 'Click me to begin game!', { fill: '#fff' })
+      .setInteractive()
+      .on('pointerdown', () => this.beginGame() );
+  }
+  beginGame() {
+    this.timedEvent = this.time.addEvent(40000, console.log('hey'), this);
+    this.socket.emit('createBall');
+    this.socket.emit('destroyButton');
+    clickButton.destroy();
+  }
+  createBall(ballInfo) {
+    ball = this.physics.add.sprite(ballInfo.x,ballInfo.y,'ball').setScale(0.5);    
+    ball.enableBody = true;    
+    ball.setVelocity(0,-250);
+    ball.allowRotation = true;
+    ball.setCollideWorldBounds(true);
+    this.physics.add.overlap(this.player, ball, this.ballHitPaddle, null, this);
+  }
+
+  createPlayer(playerInfo) {
+    // our player sprite created through the physics system
+    this.player = this.physics.add.sprite(playerInfo.x, playerInfo.y, 'player'/*, 6*/);
+    this.player.enableBody = true;
+    this.player.setCollideWorldBounds(true)
+
+
+  }
+  
+  addOtherPlayers(playerInfo) {
+    const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player'/*, 9*/);
+    otherPlayer.playerId = playerInfo.playerId;
+    this.otherPlayers.add(otherPlayer);
+  }
+  randomNum(min, max){
+	  return Math.random() * (max - min) + min;
+  }
+
+  randomInt(min, max){
+	  return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  ballHitPaddle(paddle1, ball){
+    ballFirstHit = true;
+    var bottomSide;
+    if (ball.body.y > 300) {
+      bottomSide = true;
+    } else if (ball.body.y <300) {
+      bottomSide = false;
+    }
+    var ballLocation = {
+      x: ball.body.x,
+      y: ball.body.y,
+      ifBottom:bottomSide
+    };
+    if(!notified) {
+      this.socket.emit('ballHitPaddle', ballLocation);
+      notified = true;
+    }
+  }
+
+  endGame() {
+    var endGameText = this.add.text(300, 300, 'Game Over!', { fill: '#fff' });
+      if (bottom) {
+        score = bottomScore;
+      } else {
+        score = topScore;
+      }
+      $.ajax({
+        type: 'GET',
+        url: '/highScore',
+        data: {
+          playerId: this.player.playerId,
+          refreshToken: getCookie('refreshJwt')
+        },
+        success: function(data) {
+          console.log(data.score.highScore);
+          var highScore = data.score.highScore;
+          if (highScore < score) {
+            console.log('need to update');
+            $.ajax({
+              type: 'POST',
+              url: '/updateScore',
+              data: {
+                score,
+                refreshToken: getCookie('refreshJwt')
+              },
+              success: function(data) {console.log('succeeded in update')},
+              error: function(xhr) {
+                console.log(xhr);
+              }
+            })
+          } else {
+            console.log('do not update');
+          }
+        },
+        error: function(xhr) {
+          console.log(xhr);
+        }
+      })
+      $.ajax({
+        type: 'GET',
+        url: '/scoreboard',
+        data: {
+          refreshToken: getCookie('refreshJwt')
+        }, success: function(data) { window.location.replace('/scoreboard.html');},
+        error: function(xhr) {
+          console.log(xhr);
+        }
+      })
+  }
+
+  update(time,delta) {
+    if (bottomScore - topScore > 10 || topScore-bottomScore > 10) {
+      if (!gameWon) {
+        gameWon = true;
+        this.gameOver();
+      }
+    }
+    if (ball) {
+      if (ball.body.y > 50 && ball.body.y < 550) {
+        pointWon = false;
+      }
+      this.socket.emit('ballLocation');
+      // if (ballFirstHit) {
+      //   this.socket.emit('ballLocation');
+      // } else {
+      //   this.socket.emit('diffBallLocation');
+      // }
+      if (ball.body.y <= 30) {
+        this.socket.emit('pointWon');
+        this.socket.emit('ballRestart',{x:0,y:250, bottom: true});
+      } else if (ball.body.y >= 570) {
+        this.socket.emit('pointWon');
+        this.socket.emit('ballRestart',{x:0,y:250, bottom: false});
+      }
+    }
+    if (this.player) {
+      var x = this.player.x;
+      if (this.player.oldPosition && x!= this.player.oldPosition.x) {
+        this.socket.emit('playerMovement', x);
+      }
+      this.player.oldPosition = {
+        x: this.player.x,
+      };
+    }
+
+    if (cursors.left.isDown) {
+      this.player.x -= delta/4;
+    } else if (cursors.right.isDown) {
+      this.player.x += delta/4;
+    } 
+    if (updateX && updateY) {
+      ball.setPosition(updateX,updateY);
+      ballX=updateX;
+      ballY=updateY;
+    }
+  }
+
+  gameOver() {
+    this.socket.emit('gameOver');
+    console.log('game is ending');
+  }
+  
+  check() {
+    if (this.player.y == 50) {
+      if ((ball.body.y <=50)&&((ball.body.x < (this.player.x - 75)) ||(ball.body.x> (this.player.x+75)))) {
+        this.socket.emit('ballRestart',{x:0,y:250,bottom:false});
+        bottomScore++;
+        bottomScoreText.setText('Score: ' + bottomScore);
+      }
+    } else {
+      if((ball.body.y >= 550) && ((ball.body.x < (this.player.x - 75)) || (ball.body.x > (this.player.x+75)))) {
+        this.socket.emit('ballRestart',{x:0,y:250, bottom: true});
+        topScore++;
+        topScoreText.setText('Score: ' + topScore);
+      }
+    }
+  }
+}
+
+var config = {
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0
+      },
+      debug: true // set to true to view zones
+    }
+  },
+  scene: [
+    BootScene,
+    WorldScene
+  ]
+};
+var game = new Phaser.Game(config);
