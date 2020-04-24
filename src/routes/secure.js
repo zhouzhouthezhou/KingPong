9dc30004f2845ef592aa2d6d9ec43ff9a4255bbb
ergoodwill <35115515+ergoodwill@users.noreply.github.com>
2020-04-23 20:50:05 -0600

    Documented Code

---
+++ src/routes/secure.js
@@ -0,0 +1,19 @@
+//from: https://phasertutorials.com/creating-a-phaser-3-leaderboard-with-user-authentication-using-node-js-express-mongodb-part-1/
+const express = require('express');
+const asyncMiddleware = require('../middleware/asyncMiddleware');
+const UserModel = require('../models/userModel');
+
+const router = express.Router();
+
+router.post('/submit-score', asyncMiddleware(async (req, res, next) => {
+  const { email, score } = req.body;
+  await UserModel.updateOne({ email }, { highScore: score });
+  res.status(200).json({ status: 'ok' });
+}));
+
+router.get('/scores', asyncMiddleware(async (req, res, next) => {
+  const users = await UserModel.find({}, 'name highScore -_id').sort({ highScore: -1}).limit(10);
+  res.status(200).json(users);
+}));
+
+module.exports = router;
